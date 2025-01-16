b9568f5b02266d90328216763a51fcc3fdc87b5f
gitcodingrank <jiraja2915@gmail.com>
2025-01-16 20:18:59 +0530

    Done

---
+++ G32/Unit 1/Sprint 1/Day 2/nameGreeting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //Task: Display your name within frame created using special characters/symbols
+
+    cout << "----&---**-----&---**-----&---**-----&---**-----&---**-----"<< endl;
+    cout << "I'm Anand Singh Yadav";
+
+}
