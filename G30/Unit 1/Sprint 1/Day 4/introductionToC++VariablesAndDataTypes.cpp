68dd1e64e39ebb3de6c4ae4880eb6c08ea84af81
gitcodingrank <jiraja2915@gmail.com>
2025-01-18 11:44:09 +0530

    Done

---
+++ G30/Unit 1/Sprint 1/Day 4/introductionToC++VariablesAndDataTypes.cpp
@@ -0,0 +1,49 @@
+#include <iostream> //cin, cout, other important objects.
+using namespace std;
+
+//Global Variables
+
+int main(){
+
+    //Local Variables
+    //Write your code here
+    cout <<"Hello World" <<endl;
+    //<< stream insertion operator
+
+    //varibles: its like container to store the data.
+    //syntax: data_type variableName = value;
+
+    // int age = 23;
+    // cout <<age; //any random garbage value, therefore it is important to initialize the variable at the time of creation.
+
+    float score = 23.45;
+
+    //2. It contains alphnumeric characters, underscore, and $
+
+    int number; 
+
+    //3. Avoid taking reserverd/keywords/pre-defineds words by c++.
+    int If;
+
+    //4. case-senstive: variable as age is different from variable Age
+
+    int age;
+    int Age;
+
+    //NamingConvetion while taking the varibale name.
+}
+
+
+/*
+
+python
+def checkEven(num):
+    return num%2==0
+
+
+function checkEven(num){
+    return num%2==0
+}
+
+
+*/
