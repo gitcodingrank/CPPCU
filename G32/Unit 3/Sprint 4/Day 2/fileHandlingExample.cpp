5f2bc6695197195956bbd708d79d7fd8bb76cd82
gitcodingrank <jiraja2915@gmail.com>
2025-03-27 14:50:31 +0530

    Done

---
+++ G32/Unit 3/Sprint 4/Day 2/fileHandlingExample.cpp
@@ -0,0 +1,43 @@
+//File Handling: The process to work with file(reading the and writing the file).
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+
+    // //writing the file
+    // ofstream fout("data.txt"); //Opened with file for writing
+    // fout << "Hello World!"; //Writing Hello World to the data.txt file 
+    // fout.close();
+
+    //writing the file
+    // ofstream fout("data.txt", ios::app); //Opened with file for writing
+    // fout << "Hello Everyone!"; 
+    // fout << "\nHello Everyone!"; 
+    // fout << "\nHello Everyone!"; 
+    // fout << "\nHello Everyone!"; 
+    // fout.close();
+
+    //Reading the file
+    // ifstream fin("data.txt");
+    // string data1;
+    // string data2;
+    // string data3;
+    // fin >> data;
+    // getline(fin, data1);
+    // getline(fin, data2);
+    // getline(fin, data3);
+    // cout <<data1<<endl;
+    // cout <<data2<<endl;
+
+    // while(!fin.eof()){
+    //     string data;
+    //     getline(fin, data);
+    //     cout <<data<<endl;
+    // }
+    // // cout <<data3<<endl;
+    
+    // fin.close();
+
+}
