76bb5f436b67163d42f8f56059ff80a1e04c5d2b
gitcodingrank <jiraja2915@gmail.com>
2025-03-28 20:27:29 +0530

    Done

---
+++ G32/Unit 3/Sprint 4/Day 4/libraryApp.cpp
@@ -0,0 +1,63 @@
+/*Task - Library Management System:
+
+Run->
+Welcome to Library Management System!
+
+1. Add New Book
+2. Search By Book Id
+3. Get All Books
+4. Update BooK by Id
+5. Delete Book by Id
+6. Exit
+
+Enter Your Choice: 1
+
+Enter Unique Book Id: 12121
+Enter Book Title: Book1
+Enter Book Author: Author1
+
+Book has created successfully!
+
+1. Add New Book
+2. Search By Book Id
+3. Get All Books
+4. Update BooK by Id
+5. Delete Book by Id
+6. Exit
+
+Enter Your Choice: 2
+
+Enter Book Id: 12121
+---------------------------------------------------------------------------------
+Book Details: Id: 12121, Title: Book1, Author: Author1, status: Available/Pending
+---------------------------------------------------------------------------------
+
+1. Add New Book
+2. Search By Book Id
+3. Get All Books
+4. Update BooK by Id
+5. Delete Book by Id
+6. Exit
+
+Enter Your Choice: 2
+
+Enter Book Id: 34232
+
+---------------------------------------------------------------------------------
+Invalid Book Id: Kindly Enter the correct book id
+---------------------------------------------------------------------------------
+
+1. Add New Book
+2. Search By Book Id
+3. Get All Books
+4. Update BooK by Id
+5. Delete Book by Id
+6. Exit
+
+Enter Your Choice: 6
+
+Do you want to exit(y/n)? : Y
+---------------------------------------------------------------------------------
+Thank you for using the application!
+---------------------------------------------------------------------------------
+*/
